fa261b27fd33ec3ccccd508803419437b4333e8f
indra sekhar <chebattinaindrasekhar@gmail.com>
2026-08-07 17:07:08 +0000

    Implemented resume upload and PDF text extraction

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-import numpy as np 
-import pandas as pd 
-import sklearn
-import streamlit
-import flask
-print("enviroment ready")
+from flask import Flask, render_template, request
+import os
+from utils.pdf_parser import extract_text
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "resumes"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files["resume"]
+
+    if file.filename == "":
+        return "No file selected"
+
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+    file.save(filepath)
+
+    resume_text = extract_text(filepath)
+
+    return render_template(
+        "result.html",
+        filename=file.filename,
+        resume_text=resume_text
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
